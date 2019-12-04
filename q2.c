6c0b919fe4f00439b675f5fb13798db4acdfa86c
Aaron Kim <akim106@wch129-12.cs.ucr.edu>
2019-12-04 14:47:50 -0800

    Finished q2.c

--- q2.c
+++ q2.c
@@ -7,28 +7,28 @@ double step;
 
 void main () {
   int i;
-  double pi, sum[16];
+  double pi, sum;
 
   step = 1.0/(double)num_steps;
   double start, end;
   
   start = omp_get_wtime();
-  omp_set_num_threads(16);
+  omp_set_num_threads(16); 
 
   #pragma omp parallel 
   {
     int id = omp_get_thread_num();
     int num = omp_get_num_threads();
     double x;
-    for (int j = id; j < num_steps; j += num) {
-      x = (j + 0.5) * step;
-      sum[id] = sum[id] + 4.0 / (1.0 + x * x);
+    #pragma omp for reduction(+:sum)
+    for (i = 0; i < num_steps; i++) {
+      x = (i + 0.5) * step;
+      sum = sum + 4.0 / (1.0 + x * x);
     }
     
   }
-  for (i = 0, pi = 0; i < 16; i++) {
-    pi += sum[i] * step;
-  } 
+  pi = sum * step;
+   
   end = omp_get_wtime() - start;
   printf("pi = %f, Runtime = %f \n", pi, end);
 
